e68ae8f614dd6360e184c7fb014b982adfe45470
xiuhonglee <xiuhong@mogujie.com>
2016-04-21 17:47:56 +0800

    Add route

--- public/js/monitor/createMonitor.js
+++ public/js/monitor/createMonitor.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-	var $submit = $('button[type="submit"]');
-	$submit.on('click', function() {
-		alert('lk');
-	});
+
+	var $submit = $('button[type="submit"]'),
+		$targetUrl = $('#targetUrl');
+
+
+	// 表单验证，放一放再弄
+	// $submit.on('click', function(evt) {
+	// 	evt.preventDefault();
+	// 	var $tParent = $targetUrl.parent();
+	// 	if (!$targetUrl.val()) {
+	// 		if (!$tParent.hasClass('has-error')) {
+	// 			$tParent.addClass('has-error');
+	// 		}
+	// 	} else {
+	// 		if ($tParent.hasClass('has-error')) {
+	// 			$tParent.removeClass('has-error');
+	// 		}
+	// 	}
+	// });
+
+
 });
--- routes/route.js
+++ routes/route.js
@@ -1,26 +1,60 @@
 module.exports = function(app) {
 	
-	// 组件-table
+	// view: 组件-table
 	app.get('/components/table', function(req, res) {
-		res.render('components/table', {});
+		res.render('components/table', {
+			title: 'Table组件'
+		});
 	});
-	// 组件-form
+	// view: 组件-form
 	app.get('/components/forms', function(req, res) {
-		res.render('components/forms', {});
+		res.render('components/forms', {
+			title: 'Forms组件'
+		});
 	});
-	// 组件-button
+	// view: 组件-button
 	app.get('/components/buttons', function(req, res) {
-		res.render('components/buttons', {});
+		res.render('components/buttons', {
+			title: 'Buttons组件'
+		});
 	});
 
-	// index page
+	// view: index page
 	app.get('/', function(req, res) {
-		res.render('index', {});
+		res.render('index', {
+			title: '前端自动化监控平台'
+		});
 	});
 
-	// index page
-	app.get('/monitor/createMonitor', function(req, res) {
-		res.render('monitor/createMonitor', {});
+	// view: index page
+	app.get('/monitor/list', function(req, res) {
+		res.render('index', {
+			title: '监控列表'
+		});
 	});
+
+	// view: create monitor page
+	app.get('/monitor/createMonitor', function(req, res) {
+		res.render('monitor/createMonitor', {
+			title: '创建监控项',
+			monitor: {
+				targetUrl: '',
+				targetName: ''
+			}
+		});
+	});
+
+	// control: create monitor
+	app.post('/control/monitor/createMonitor', function(req, res) {
+		
+
+	});
+
 
 };
+
+
+
+
+
+
